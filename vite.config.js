82d31a0e3e4ad82ac31c6bc351a926e4e1e88f71
saiyam1301 <shreekrishnaudupa421@gmail.com>
2025-02-18 21:07:06 +0530

    Ignore

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/saiyam1301/',  // Change this to your repo name or subfolder name
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
