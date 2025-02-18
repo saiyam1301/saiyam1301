dfa1a900a92b64e13c04ded699a7539ea56562a9
saiyam1301 <shreekrishnaudupa421@gmail.com>
2025-02-18 23:01:19 +0530

    Ignore

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/saiyam1301/', // Ensure this is correct
+  base: '/', // Ensure this is correct
   plugins: [react()],
 });
